d6873bb4882562f1ad92e12ee57a738e13cb9580
domberardi10 <domberardi10@gmail.com>
2022-12-04 16:32:21 -0800

    Added emissive control script and some particles

---
+++ Assets/Scripts/EmissionControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionControl : MonoBehaviour
+{
+
+    public float emissionTimer = 0f;
+    private Material myMaterial;
+    private Vector4 currentColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        currentColor = myMaterial.GetColor("_EmissionColor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        emissionTimer += Time.deltaTime;
+        if (emissionTimer < 5f){
+            myMaterial.SetColor("_EmissionColor", currentColor * 0.999f);
+            currentColor = currentColor * 0.999f;
+        }
+        if (emissionTimer > 5f){
+            myMaterial.SetColor("_EmissionColor", currentColor * 1.001f);
+            currentColor = currentColor * 1.001f;
+        }
+        if (emissionTimer >= 10f){
+            emissionTimer = 0f;
+        }
+    }
+}
